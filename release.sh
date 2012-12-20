3463cf5c48ef594f21fdcc6902ef49383eb9d102
Jeff Simpson <jeff.simpson@livingsocial.com>
2012-12-20 16:22:49 -0700

    Updated release script to default java exe to arch linux defualt. $JAVA_HOME should override

--- release.sh
+++ release.sh
@@ -10,7 +10,7 @@ cat << 'EOF' > $OUTPUT_FILE
 #!/bin/sh
 MYSELF=`which "$0" 2>/dev/null`
 [ $? -gt 0 -a -f "$0" ] && MYSELF="./$0"
-java=java
+java=/opt/java/bin/java
 if test -n "$JAVA_HOME"; then
     java="$JAVA_HOME/bin/java"
 fi
